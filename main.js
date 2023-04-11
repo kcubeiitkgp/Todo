e5369340866f50d3fa77c48a4f46f96aeda6884b
kcubeiitkgp <62806768+kcubeiitkgp@users.noreply.github.com>
2023-04-11 15:04:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,70 +1,71 @@
-// Select the form and the task list
 const form = document.querySelector('form');
-const taskList = document.querySelector('ul');
+const taskList = document.querySelector('#taskList');
+const clearBtn = document.querySelector('#clearBtn');
 
-// Add tasks from local storage
+// Load tasks from local storage
 if (localStorage.getItem('tasks')) {
   const tasks = JSON.parse(localStorage.getItem('tasks'));
   for (const task of tasks) {
-    const taskItem = document.createElement('li');
-    taskItem.innerText = task;
+    const taskItem = createTaskElement(task);
     taskList.appendChild(taskItem);
   }
 }
 
-// Function to handle form submission
+// Add a new task
 function addTask(event) {
-  // Prevent the default form submission behavior
   event.preventDefault();
-
-  // Get the input field value and create a new task list item
   const newTask = document.querySelector('#newTask').value.trim();
   if (newTask !== '') {
-    const newTaskItem = document.createElement('li');
-    newTaskItem.innerText = newTask;
-
-    // Add the new task to the task list
-    taskList.appendChild(newTaskItem);
-
-    // Save tasks to local storage
-    const tasks = [];
-    for (const taskItem of taskList.querySelectorAll('li')) {
-      tasks.push(taskItem.innerText);
-    }
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-
-    // Clear the input field
+    const taskItem = createTaskElement(newTask);
+    taskList.appendChild(taskItem);
+    saveTasks();
     document.querySelector('#newTask').value = '';
   }
 }
 
-// Add an event listener to the form
-form.addEventListener('submit', addTask);
+// Create a task element
+function createTaskElement(task) {
+  const taskItem = document.createElement('li');
+  taskItem.innerText = task;
+  taskItem.addEventListener('click', toggleCompleted);
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerText = 'X';
+  deleteBtn.addEventListener('click', deleteTask);
+  taskItem.appendChild(deleteBtn);
+  return taskItem;
+}
 
-// Add event listeners to task list items to mark them as completed
-taskList.addEventListener('click', function(event) {
-  const target = event.target;
-  if (target.tagName === 'LI') {
-    target.classList.toggle('completed');
-    // Save tasks to local storage
-    const tasks = [];
-    for (const taskItem of taskList.querySelectorAll('li')) {
-      tasks.push(taskItem.innerText);
-    }
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }
-});
+// Toggle completed status
+function toggleCompleted() {
+  this.classList.toggle('completed');
+  saveTasks();
+}
 
-// Function to clear all completed tasks
+// Delete task
+function deleteTask() {
+  this.parentNode.remove();
+  saveTasks();
+}
+
+// Clear completed tasks
 function clearCompletedTasks() {
   const completedTasks = taskList.querySelectorAll('.completed');
   for (const taskItem of completedTasks) {
     taskItem.remove();
   }
-  // Save tasks to local storage
+  saveTasks();
+}
+
+// Save tasks to local storage
+function saveTasks() {
   const tasks = [];
   for (const taskItem of taskList.querySelectorAll('li')) {
     tasks.push(taskItem.innerText);
   }
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
+
+// Event listeners
+form.addEventListener('submit', addTask);
+clearBtn.addEventListener('click', clearCompletedTasks);
+
